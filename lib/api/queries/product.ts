b7a1d5d2fd34c31f0ab6eddf9c5457c0c0ffe09f
robboten <robboten@gmail.com>
2024-05-05 20:51:44 +0200

    product api first parameter added

--- lib/api/queries/product.ts
+++ lib/api/queries/product.ts
@@ -1,11 +1,15 @@
 import productFragment from "../fragments/product";
 
 export const getProductsQuery = /* GraphQL */ `
-  query getProducts($order_by: [ProductSortInput!], $query: String!) {
+  query getProducts(
+    $order_by: [ProductSortInput!]
+    $query: String!
+    $first: Int
+  ) {
     products(
       order: $order_by
       where: { title: { contains: $query } }
-      first: 100
+      first: $first
     ) {
       edges {
         node {
