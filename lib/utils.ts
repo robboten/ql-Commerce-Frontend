262f4cf2ac554ba398996180a859e8a3c9947eb7
robboten <robboten@gmail.com>
2024-05-06 00:07:48 +0200

    alt price function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,35 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+//https://github.com/saleor/storefront/blob/main/src/app/%5Bchannel%5D/(main)/products/%5Bslug%5D/page.tsx
+
+export const formatDate = (date: Date | number) => {
+  return new Intl.DateTimeFormat("en-US", { dateStyle: "medium" }).format(date);
+};
+
+export const formatMoney = (amount: number, currency: string) =>
+  new Intl.NumberFormat("sv-se", {
+    style: "currency",
+    currency,
+  }).format(amount);
+
+export const formatMoneyRange = (
+  range: {
+    start?: { amount: number; currency: string } | null;
+    stop?: { amount: number; currency: string } | null;
+  } | null
+) => {
+  const { start, stop } = range || {};
+  const startMoney = start && formatMoney(start.amount, start.currency);
+  const stopMoney = stop && formatMoney(stop.amount, stop.currency);
+
+  if (startMoney === stopMoney) {
+    return startMoney;
+  }
+
+  return `${startMoney} - ${stopMoney}`;
+};
