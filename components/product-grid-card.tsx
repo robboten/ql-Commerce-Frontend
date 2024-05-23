0f26b6fac700c220de4b0d1dcec2465a0b68cd6c
robboten <robboten@gmail.com>
2024-05-23 18:41:50 +0200

    refactoring of product grid/card

---
+++ components/product-grid-card.tsx
@@ -0,0 +1,36 @@
+import Price from "@/components/price";
+import { getProducts } from "@/lib/api";
+import { Product } from "@/lib/api/types";
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function ProductGridCard({
+  product,
+}: {
+  product: Product;
+}) {
+  const body = product.description.split("\n");
+  return (
+    <Link
+      href={`/product/${product.handle}`}
+      className="grid grid-rows-subgrid row-span-4 h-full gap-0"
+    >
+      <div className="relative w-full aspect-square lg:aspect-[2/3] object-cover">
+        <Image
+          src={product.featuredImage.url}
+          alt="alt"
+          fill
+          // width={300}
+          // height={300}
+          className="object-cover"
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        />
+      </div>
+      <h3 className="mt-2 mb-1 text-xl">{product.title}</h3>
+      <span className="font-semibold mb-2 text-xl">
+        <Price amount={product.priceRange.minVariantPrice} className="inline" />
+      </span>
+      <p className="text-sm">{body[0]}</p>
+    </Link>
+  );
+}
--- components/product-grid.tsx
+++ components/product-grid.tsx
@@ -1,8 +1,5 @@
-import Price from "@/components/price";
-import { getProducts } from "@/lib/api";
 import { Product } from "@/lib/api/types";
-import Image from "next/image";
-import Link from "next/link";
+import ProductGridCard from "./product-grid-card";
 
 export default async function ProductGrid({
   products,
@@ -11,33 +8,8 @@ export default async function ProductGrid({
 }) {
   return (
     <div className="grid grid-cols-auto-fit-100 auto-rows-auto grid-flow gap-x-3 gap-y-10 w-full">
-      {products.map((f, i) => {
-        const body = f.description.split("\n");
-
-        return (
-          <Link
-            key={i}
-            href={`/product/${f.handle}`}
-            className="grid grid-rows-subgrid row-span-4 h-full gap-0"
-          >
-            <div className="relative w-full aspect-square lg:aspect-[2/3] object-cover">
-              <Image
-                src={f.featuredImage.url}
-                alt="alt"
-                fill
-                // width={300}
-                // height={300}
-                className="object-cover"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              />
-            </div>
-            <h3 className="mt-2 mb-1 text-xl">{f.title}</h3>
-            <span className="font-semibold mb-2 text-xl">
-              <Price amount={f.priceRange.minVariantPrice} className="inline" />
-            </span>
-            <p className="text-sm">{body[0]}</p>
-          </Link>
-        );
+      {products.map((product) => {
+        return <ProductGridCard key={product.id} product={product} />;
       })}
     </div>
   );
